aeba907177895fbdf3851fe768669cc8f00b3b6b
Iwinter78 <kille1404.so@gmail.com>
2024-12-05 12:40:54 +0100

    Added route for adding user

--- restapi/index.js
+++ restapi/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { createUser } from './src/user.js';
 
 const app = express();
 
@@ -6,6 +7,13 @@ const port = 1337;
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
+});
+
+app.post('/api/v1/create/user/:email/:username', async (req, res) => {
+    const email = req.params.email;
+    const username = req.params.username;
+    const response = await createUser(username, email);
+    res.json({ response });
 });
 
 app.listen(port, () => {

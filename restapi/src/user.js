73e68b1cbbcbc343d6191c5460072fd21595c4a1
Iwinter78 <kille1404.so@gmail.com>
2024-12-06 16:03:48 +0100

    Refactored the code. Did not need to be so complicated

--- restapi/src/user.js
+++ restapi/src/user.js
@@ -4,15 +4,8 @@ async function createUser(username, email) {
     const db = await connect();
     const query = `CALL create_user(?, ?)`;
     const values = [username, email];
-    return new Promise((resolve, reject) => {
-        db.query(query, values, (error, results) => {
-            db.end();
-            if (error) {
-                reject(error);
-            }
-            resolve(results);
-        });
-    });
+    await db.query(query, values);
+    db.end();
 }
 
 

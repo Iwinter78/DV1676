e284ef8c377bffb9ca44b5341d860b74fb119129
Iwinter78 <kille1404.so@gmail.com>
2024-12-09 13:50:29 +0100

    Added comment

--- restapi/src/connect.js
+++ restapi/src/connect.js
@@ -1,5 +1,9 @@
 import mysql from 'mysql';
 
+/**
+ * Creates a connection to the database
+ * @returns {Promise<mysql.Connection>}
+ */
 export async function connect() {
     const connect = mysql.createConnection({
         host: 'database-dev',

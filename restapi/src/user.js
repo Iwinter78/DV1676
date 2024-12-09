50464e790b49e8add86f09b92cdac8742d3ff20c
Iwinter78 <kille1404.so@gmail.com>
2024-12-09 13:39:26 +0100

    Added comments and a function for getting the user data

--- restapi/src/user.js
+++ restapi/src/user.js
@@ -1,5 +1,11 @@
+'use strict';
 import { connect } from './connect.js';
 
+/**
+ * Creates a new user and stores it in the database
+ * @param {String} username - The username of the user
+ * @param {String} email - The email of the user 
+ */
 async function createUser(username, email) {
     const db = await connect();
     const query = `CALL create_user(?, ?)`;
@@ -8,5 +14,24 @@ async function createUser(username, email) {
     db.end();
 }
 
+/**
+ * Retrives a user from the database
+ * @param {String} email - The email of the user
+ * @returns {Promise<mysql.RowDataPacket[]>} - Returns a promise with the infomation of the user
+ */
+async function getUser(email) {
+    const db = await connect();
+    const query = `CALL get_user(?)`;
+    const values = [email];
+    const result = await db.query(query, values);
+    db.end();
+    return result;
+}
 
-export { createUser };
+
+
+
+export { 
+    createUser,
+    getUser
+};

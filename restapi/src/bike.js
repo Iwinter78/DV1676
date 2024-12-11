b5cd301736ccd7d788a79c3a6c443d73c755b051
Iwinter78 <kille1404.so@gmail.com>
2024-12-11 17:31:11 +0100

    Added functions for calling the procedures

--- restapi/src/bike.js
+++ restapi/src/bike.js
@@ -1,7 +1,30 @@
 import { connect } from './connect.js';
 
-async function createBike() {
-    const db = await connect();
+const db = await connect();
+
+async function createBike(gps, city) {
+    const query = 'CALL create_bike(?, ?)';
+    const values = [gps, city];
+    db.query(query, values);
+    db.end();
 }
 
-export { createBike };
+async function getAllBikesPosition() {
+    const query = 'CALL get_all_bikes_position()';
+    const response = await db.query(query);
+    db.end();
+    return response[0];
+}
+
+async function updateBikePosition(id, gps) {
+    const query = 'CALL update_bike_position(?, ?)';
+    const values = [id, gps];
+    db.query(query, values);
+    db.end();
+}
+
+export { 
+    createBike,
+    getAllBikesPosition,
+    updateBikePosition
+};

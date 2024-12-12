02fb7d93af6b39b22c35db8c4ea5f469c65b2d8b
Iwinter78 <kille1404.so@gmail.com>
2024-12-12 10:58:31 +0100

    Added comments

--- restapi/src/bike.js
+++ restapi/src/bike.js
@@ -1,14 +1,21 @@
 import { connect } from './connect.js';
 
 const db = await connect();
-
+/**
+ * Creates a bike and stores it in the database
+ * @param {String} gps - Gps coordinates for the bike 
+ * @param {*} city - The city where the bike is located
+ */
 async function createBike(gps, city) {
     const query = 'CALL create_bike(?, ?)';
     const values = [gps, city];
     db.query(query, values);
     db.end();
 }
-
+/**
+ * Retrives all the bikes and their positions
+ * @returns {Array} - An array containing all bikes and their positions
+ */
 async function getAllBikesPosition() {
     const query = 'CALL get_all_bikes_position()';
     const response = await db.query(query);
@@ -16,6 +23,11 @@ async function getAllBikesPosition() {
     return response[0];
 }
 
+/**
+ * Updates the bikes position
+ * @param {String} id - The id of the bike
+ * @param {String} gps - The gps coordinates of the bike
+ */
 async function updateBikePosition(id, gps) {
     const query = 'CALL update_bike_position(?, ?)';
     const values = [id, gps];

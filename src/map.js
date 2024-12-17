a3bcba800b7aadf26e55aca1312a20e75f6101f3
Iwinter78 <kille1404.so@gmail.com>
2024-12-17 14:41:27 +0100

    Markers now show where the bikes are on the map

--- src/map.js
+++ src/map.js
@@ -1,6 +1,7 @@
 import L from "leaflet";
-//import { decode, encode } from 'open-location-code';
+import { OpenLocationCode } from 'open-location-code';
 document.addEventListener('DOMContentLoaded', () => {
+    const openLocationCode = new OpenLocationCode();
     const locateButton = document.getElementById('locate-user');
     const cityDropdown = document.getElementById('city-select');
 
@@ -16,6 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 iconAnchor: [10, 10]
             });
 
+            const bikeIcon = L.divIcon({
+                className: 'custom-marker',
+                html: '<div style="font-size: 30px; color: #00ff00; font-weight: bold;">🚲</div>',
+                iconSize: [20, 20],
+                iconAnchor: [10, 10]
+            })
+
             const map = L.map('map').setView([latitude, longitude], 16);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 18,
@@ -25,7 +33,31 @@ document.addEventListener('DOMContentLoaded', () => {
             L.marker([latitude, longitude], {icon: customIcon}).addTo(map)
                 .bindPopup("Här är du!")
                 .openPopup();
+            
+            let bikeCordinates = fetch("http://localhost:1337/api/v1/bike", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: 'include'
+            })
+                .then((response) => response.json())
+            
+            
 
+            bikeCordinates.then((data) => {
+                data.forEach((bike) => {
+                    let findCode = openLocationCode.recoverNearest(bike.gps, 56.1, 15.5);
+                    let decodedCordinates = openLocationCode.decode(findCode);
+                    let latitude = decodedCordinates.latitudeCenter;
+                    let longitude = decodedCordinates.longitudeCenter;
+                    
+                    console.log(latitude, longitude);
+                    L.marker([latitude, longitude], {icon: bikeIcon}).addTo(map)
+                        .bindPopup("Här är en cykel!")
+                        .openPopup();
+                });
+            });
             locateButton.addEventListener('click', () => {
                 map.locate({ setView: true, maxZoom: 16 });
             });

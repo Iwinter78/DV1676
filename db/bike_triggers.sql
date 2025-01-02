2373845c12c3fdb223b3d666130dac9d5fb014cb
Iwinter78 <kille1404.so@gmail.com>
2025-01-02 15:27:48 +0100

    Added trigger for bike log. It now logs as soon as table is updated

---
+++ db/bike_triggers.sql
@@ -0,0 +1,15 @@
+CREATE TRIGGER `bike_log_trigger` AFTER UPDATE ON `bike`
+FOR EACH ROW
+BEGIN
+    INSERT INTO bike_log (bike_id, log_time, log_data, log_userid)
+    VALUES (
+        OLD.id,
+        CURRENT_TIMESTAMP,
+        CONCAT(
+            'GPS: ', OLD.gps,
+            ', City: ', OLD.city,
+            ', Status: ', OLD.bike_status
+        ),
+        OLD.currentuser
+    );
+END;

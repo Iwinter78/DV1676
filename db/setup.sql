25d60e2f9478105dcbd44bbd8b83ee9884196224
Iwinter78 <kille1404.so@gmail.com>
2025-01-03 18:58:03 +0100

    Changed order of files

--- db/setup.sql
+++ db/setup.sql
@@ -7,8 +7,8 @@ SOURCE ddl.sql;
 SOURCE user_procedures.sql;
 SOURCE bike_procedures.sql;
 SOURCE station_procedures.sql;
+--SOURCE bike_triggers.sql
 SOURCE insert.sql
 SOURCE insert_bike_city_station.sql
-SOURCE bike_triggers.sql
 
 

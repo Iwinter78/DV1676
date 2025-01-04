eae2c6418b89642e60b77b6d29af7056747ab98b
Filippa Lissäng Öst <filippaliss@gmail.com>
2025-01-04 16:44:10 +0100

    moved the log procedures to this file

---
+++ db/log_procedures.sql
@@ -0,0 +1,36 @@
+DROP PROCEDURE IF EXISTS get_user_log;
+DROP PROCEDURE IF EXISTS show_user_logs;
+
+DROP PROCEDURE IF EXISTS show_bike_logs;
+DROP PROCEDURE IF EXISTS show_station_logs;
+DROP PROCEDURE IF EXISTS show_bank_logs;
+
+DELIMITER ;;
+CREATE PROCEDURE get_user_log(IN username_param VARCHAR(255))
+BEGIN
+    SELECT u.id AS user_id, u.username, ul.log_time, ul.log_data
+    FROM users u
+    JOIN user_log ul ON u.id = ul.id
+    WHERE u.username = username_param;
+END;;
+
+CREATE PROCEDURE show_user_logs()
+BEGIN
+    SELECT * FROM user_log;
+END;;
+
+CREATE PROCEDURE show_bike_logs()
+BEGIN
+    SELECT * FROM bike_log;
+END;;
+
+CREATE PROCEDURE show_station_logs()
+BEGIN
+    SELECT * FROM station_log;
+END;;
+
+CREATE PROCEDURE show_bank_logs()
+BEGIN
+    SELECT * FROM bank_log;
+END;;
+DELIMITER ;

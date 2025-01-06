5b2adda964054ae97ad26fba0a7e76b6c8480694
Iwinter78 <kille1404.so@gmail.com>
2025-01-06 12:46:39 +0100

    Bike is now not avaliable during charging

--- src/map.js
+++ src/map.js
@@ -34,6 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const bookedBikeIcon = createIcon("#ff0000");
     const simIcon = createIcon("#FF7518");
     const needsAttentionIcon = createIcon("#FFA500");
+    const chargingMode = bookedBikeIcon;
 
     const map = L.map("map").setView([latitude, longitude], 16);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -78,24 +79,58 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
 
+    function isPointInStation(bikeCords, stationCords) {
+      const polygonCords = stationCords.map((cord => L.latLng(cord)))
+
+      const polygon = L.polygon(polygonCords);
+
+      const bikePoint = L.latLng(bikeCords);
+
+      return polygon.getBounds().contains(bikePoint);
+    }
+
+    async function checkBikeInAnyStation(bike) {
+      const response = await fetch("http://localhost:1337/api/v1/stations");
+      const data = await response.json();
+      const stations = data[0];
+      
+      for (const station of stations) {
+          const stationCoordinates = JSON.parse(station.gps);
+          
+          if (isPointInStation(bike, stationCoordinates)) {
+              return {
+                  isInStation: true,
+                  stationId: station.id,
+                  chargingSize: station.charging_size
+              };
+          }
+      }
+      
+      return {
+          isInStation: false,
+          stationId: null,
+          chargingSize: null
+      };
+    }
+
     async function displayBikes() {
       try {
         const data = await fetchBikes();
         const bikes = data[0];
 
-        bikes.forEach((bike) => {
+        bikes.forEach(async (bike) => {
           let lat = 0;
           let lng = 0;
 
           if (bike.city === 1) {
-            lat = 56.1;
-            lng = 15.5;
+              lat = 56.1;
+              lng = 15.5;
           } else if (bike.city === 2) {
-            lat = 59.3;
-            lng = 18.1;
+              lat = 59.3;
+              lng = 18.1;
           } else if (bike.city === 3) {
-            lat = 55.6;
-            lng = 13.0;
+              lat = 55.6;
+              lng = 13.0;
           }
 
           // Decode Open Location Code
@@ -104,36 +139,46 @@ document.addEventListener("DOMContentLoaded", () => {
           const latitude = decodedCoordinates.latitudeCenter;
           const longitude = decodedCoordinates.longitudeCenter;
 
-          if (
-            bike.currentuser === userData.id ||
-            getRole(userData.id) === "admin"
-          ) {
-            L.marker([latitude, longitude], { icon: bookedBikeIcon })
-              .addTo(map)
-              .bindPopup(
-                `Cykel: ${bike.id} <br> <a href="/book/confirm/${bike.id}">Se bokning</a>`,
-              )
-              .openPopup();
-          } else if (bike.status === 1) {
-            L.marker([latitude, longitude], { icon: needsAttentionIcon })
-              .addTo(map)
-              .bindPopup(
-                `Cykel: ${bike.id} <br> <a href="/book/confirm/${bike.id}">Flytta till laddningstation</a>`,
-              )
-              .openPopup();
+          // Check if bike is in a station
+          const stationCheck = await checkBikeInAnyStation([latitude, longitude]);
+          
+          let bikeIcon;
+          let popupContent;
+
+          if (stationCheck.isInStation) {
+              // Bike is in a charging station
+              if (bike.status === 1) {
+                  bikeIcon = needsAttentionIcon;
+                  popupContent = `Cykel: ${bike.id} <br> I laddningsstation ${stationCheck.stationId} <br> Behöver laddas`;
+              } else if (bike.status === 2) {
+                  bikeIcon = chargingMode;
+                  popupContent = `Cykel: ${bike.id} <br> Laddning pågår i station ${stationCheck.stationId}`;
+              } else {
+                  bikeIcon = availableBikeIcon;
+                  popupContent = `Cykel: ${bike.id} <br> I laddningsstation ${stationCheck.stationId} <br> <a href="/book/confirm/${bike.id}">Boka</a>`;
+              }
           } else {
-            L.marker([latitude, longitude], { icon: availableBikeIcon })
-              .addTo(map)
-              .bindPopup(
-                `Cykel: ${bike.id} <br> <a href="/book/confirm/${bike.id}">Boka</a>`,
-              )
-              .openPopup();
+              if (bike.currentuser === userData.id || getRole(userData.login) === "admin") {
+                  bikeIcon = bookedBikeIcon;
+                  popupContent = `Cykel: ${bike.id} <br> <a href="/book/confirm/${bike.id}">Se bokning</a>`;
+              } else if (bike.status === 1) {
+                  bikeIcon = needsAttentionIcon;
+                  popupContent = `Cykel: ${bike.id} <br> <a href="/book/confirm/${bike.id}">Boka</a>`;
+              } else {
+                  bikeIcon = availableBikeIcon;
+                  popupContent = `Cykel: ${bike.id} <br> <a href="/book/confirm/${bike.id}">Boka</a>`;
+              }
           }
-        });
-      } catch (error) {
+
+          L.marker([latitude, longitude], { icon: bikeIcon })
+              .addTo(map)
+              .bindPopup(popupContent)
+              .openPopup();
+      });
+    } catch (error) {
         console.error("Error displaying bikes:", error);
-      }
     }
+}
 
     displayBikes();
 

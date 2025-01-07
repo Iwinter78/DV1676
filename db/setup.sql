27a204827f0aa0a68fe245f2db6c6d39e03a3c3c
Iwinter78 <kille1404.so@gmail.com>
2025-01-07 12:16:00 +0100

    Added parking zone procedures to the setup file

--- db/setup.sql
+++ db/setup.sql
@@ -7,6 +7,7 @@ SOURCE ddl.sql;
 SOURCE user_procedures.sql;
 SOURCE bike_procedures.sql;
 SOURCE station_procedures.sql;
+SOURCE parking_zone_procedures.sql;
 SOURCE bike_triggers.sql
 SOURCE insert.sql
 SOURCE insert_bike_city_station.sql

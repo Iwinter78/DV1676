f1a1dd6f6a5b40a3d024fabf7e2785c7855ee94f
Iwinter78 <kille1404.so@gmail.com>
2025-01-09 11:40:14 +0100

    Created procedure for updating the amount of bikes

--- db/parking_zone_procedures.sql
+++ db/parking_zone_procedures.sql
@@ -1,8 +1,21 @@
 DROP PROCEDURE IF EXISTS get_all_parking_zones;
+DROP PROCEDURE IF EXISTS update_amount_of_bikes;
 
 DELIMITER ;;
 CREATE PROCEDURE get_all_parking_zones()
 BEGIN
     SELECT * FROM parking_zones;
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE PROCEDURE update_amount_of_bikes(
+    IN zone_id INT,
+    IN amount INT
+)
+BEGIN
+    UPDATE parking_zones
+    SET bikes_in_zone = amount
+    WHERE id = zone_id;
 END;;
 DELIMITER ;

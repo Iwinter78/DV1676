48ea9b1cb0857ba2f9607911b0954edcab230541
Iwinter78 <kille1404.so@gmail.com>
2025-01-13 16:11:00 +0100

    No idea why this file is not staged

--- __tests__/bike.test.js
+++ __tests__/bike.test.js
@@ -2,251 +2,251 @@ import { jest, describe, expect, test } from "@jest/globals";
 const host = "http://localhost:1337";
 
 describe("Bike features in rest api", () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-        global.fetch = jest.fn();
+  beforeEach(() => {
+    jest.clearAllMocks();
+    global.fetch = jest.fn();
+  });
+
+  test("Be able to create a bike", async () => {
+    const bike = {
+      gps: "5HJW+FGX",
+      city: "Karlskrona",
+    };
+
+    const mockResponse = {
+      message: "Cykel skapad",
+      status: 201,
+      json: () =>
+        Promise.resolve({
+          bike,
+        }),
+    };
+
+    global.fetch.mockResolvedValue(mockResponse);
+
+    const response = await fetch(`${host}/api/v1/create/bike`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Be able to create a bike", async () => {
-        const bike = {
-            gps: "5HJW+FGX",
-            city: "Karlskrona"
-        };
+    const data = await response.json();
 
-        const mockResponse = {
-            message: "Cykel skapad",
-            status: 201,
-            json: () =>
-                Promise.resolve({
-                    bike
-                }),
-        };
+    expect(response.status).toBe(201);
+    expect(response.message).toBe("Cykel skapad");
+    expect(data.bike.gps).toBe(bike.gps);
+    expect(data.bike.city).toBe(bike.city);
+  });
 
-        global.fetch.mockResolvedValue(mockResponse);
+  test("Should not create a bike when missing city", async () => {
+    const bike = {
+      gps: "5HJW+FGX",
+    };
 
-        const response = await fetch(`${host}/api/v1/create/bike`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
+    const mockResponse = {
+      message: "GPS och stad krävs",
+      status: 400,
+    };
 
-        const data = await response.json();
+    global.fetch.mockResolvedValue(mockResponse);
 
-        expect(response.status).toBe(201);
-        expect(response.message).toBe("Cykel skapad");
-        expect(data.bike.gps).toBe(bike.gps);
-        expect(data.bike.city).toBe(bike.city);
+    const response = await fetch(`${host}/api/v1/create/bike`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Should not create a bike when missing city", async () => {
-        const bike = {
-            gps: "5HJW+FGX"
-        };
+    expect(response.status).toBe(400);
+    expect(response.message).toBe("GPS och stad krävs");
+  });
 
-        const mockResponse = {
-            message: "GPS och stad krävs",
-            status: 400,
-        };
+  test("Should not create a bike when missing gps", async () => {
+    const bike = {
+      city: "Karlskrona",
+    };
 
-        global.fetch.mockResolvedValue(mockResponse);
+    const mockResponse = {
+      message: "GPS och stad krävs",
+      status: 400,
+    };
 
-        const response = await fetch(`${host}/api/v1/create/bike`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
+    global.fetch.mockResolvedValue(mockResponse);
 
-        expect(response.status).toBe(400);
-        expect(response.message).toBe("GPS och stad krävs");
+    const response = await fetch(`${host}/api/v1/create/bike`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Should not create a bike when missing gps", async () => {
-        const bike = {
-            city: "Karlskrona"
-        };
+    expect(response.status).toBe(400);
+    expect(response.message).toBe("GPS och stad krävs");
+  });
 
-        const mockResponse = {
-            message: "GPS och stad krävs",
-            status: 400,
-        };
+  test("Updates the position of a bike", async () => {
+    const bike = {
+      id: 1,
+      gps: "5HJW+FGX",
+    };
 
-        global.fetch.mockResolvedValue(mockResponse);
+    const mockResponse = {
+      message: "Cykel uppdaterad",
+      status: 200,
+      json: () =>
+        Promise.resolve({
+          bike,
+        }),
+    };
 
-        const response = await fetch(`${host}/api/v1/create/bike`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
+    global.fetch.mockResolvedValue(mockResponse);
 
-        expect(response.status).toBe(400);
-        expect(response.message).toBe("GPS och stad krävs");
+    const response = await fetch(`${host}/api/v1/update/bike`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Updates the position of a bike", async () => {
-        const bike = {
-            id: 1,
-            gps: "5HJW+FGX"
-        };
+    const data = await response.json();
 
-        const mockResponse = {
-            message: "Cykel uppdaterad",
-            status: 200,
-            json: () =>
-                Promise.resolve({
-                    bike
-                }),
-        };
+    expect(response.status).toBe(200);
+    expect(response.message).toBe("Cykel uppdaterad");
+    expect(data.bike.id).toBe(bike.id);
+    expect(data.bike.gps).toBe(bike.gps);
+  });
 
-        global.fetch.mockResolvedValue(mockResponse);
+  test("Should not update a bike when missing gps", async () => {
+    const bike = {
+      id: 1,
+    };
 
-        const response = await fetch(`${host}/api/v1/update/bike`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
+    const mockResponse = {
+      message: "GPS och id krävs",
+      status: 400,
+    };
 
-        const data = await response.json();
+    global.fetch.mockResolvedValue(mockResponse);
 
-        expect(response.status).toBe(200);
-        expect(response.message).toBe("Cykel uppdaterad");
-        expect(data.bike.id).toBe(bike.id);
-        expect(data.bike.gps).toBe(bike.gps);
+    const response = await fetch(`${host}/api/v1/update/bike`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Should not update a bike when missing gps", async () => {
-        const bike = {
-            id: 1
-        };
+    expect(response.status).toBe(400);
+    expect(response.message).toBe("GPS och id krävs");
+  });
 
-        const mockResponse = {
-            message: "GPS och id krävs",
-            status: 400,
-        };
+  test("Should not update a bike when missing id", async () => {
+    const bike = {
+      gps: "5HJW+FGX",
+    };
 
-        global.fetch.mockResolvedValue(mockResponse);
+    const mockResponse = {
+      message: "GPS och id krävs",
+      status: 400,
+    };
 
-        const response = await fetch(`${host}/api/v1/update/bike`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
+    global.fetch.mockResolvedValue(mockResponse);
 
-        expect(response.status).toBe(400);
-        expect(response.message).toBe("GPS och id krävs");
+    const response = await fetch(`${host}/api/v1/update/bike`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Should not update a bike when missing id", async () => {
-        const bike = {
-            gps: "5HJW+FGX"
-        };
+    expect(response.status).toBe(400);
+    expect(response.message).toBe("GPS och id krävs");
+  });
 
-        const mockResponse = {
-            message: "GPS och id krävs",
-            status: 400,
-        };
+  test("Should be able to book a bike", async () => {
+    const bike = {
+      id: 1,
+      user: 1,
+    };
 
-        global.fetch.mockResolvedValue(mockResponse);
+    const mockResponse = {
+      message: "Cykel bokad",
+      status: 200,
+      json: () =>
+        Promise.resolve({
+          bike,
+        }),
+    };
 
-        const response = await fetch(`${host}/api/v1/update/bike`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
+    global.fetch.mockResolvedValue(mockResponse);
 
-        expect(response.status).toBe(400);
-        expect(response.message).toBe("GPS och id krävs");
+    const response = await fetch(`${host}/api/v1/bike/book`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Should be able to book a bike", async () => {
-        const bike = {
-            id: 1,
-            user: 1
-        };
+    const data = await response.json();
 
-        const mockResponse = {
-            message: "Cykel bokad",
-            status: 200,
-            json: () =>
-                Promise.resolve({
-                    bike
-                }),
-        };
+    expect(response.status).toBe(200);
+    expect(response.message).toBe("Cykel bokad");
+    expect(data.bike.id).toBe(bike.id);
+    expect(data.bike.user).toBe(bike.user);
+  });
 
-        global.fetch.mockResolvedValue(mockResponse);
+  test("Should not be able to book a bike when missing user", async () => {
+    const bike = {
+      id: 1,
+    };
 
-        const response = await fetch(`${host}/api/v1/bike/book`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
+    const mockResponse = {
+      message: "Id och användare-id krävs",
+      status: 400,
+    };
 
-        const data = await response.json();
+    global.fetch.mockResolvedValue(mockResponse);
 
-        expect(response.status).toBe(200);
-        expect(response.message).toBe("Cykel bokad");
-        expect(data.bike.id).toBe(bike.id);
-        expect(data.bike.user).toBe(bike.user);
+    const response = await fetch(`${host}/api/v1/bike/book`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Should not be able to book a bike when missing user", async () => {
-        const bike = {
-            id: 1
-        };
+    expect(response.status).toBe(400);
+    expect(response.message).toBe("Id och användare-id krävs");
+  });
 
-        const mockResponse = {
-            message: "Id och användare-id krävs",
-            status: 400,
-        };
+  test("Should not be able to book a bike when missing id", async () => {
+    const bike = {
+      user: 1,
+    };
 
-        global.fetch.mockResolvedValue(mockResponse);
+    const mockResponse = {
+      message: "Id och användare-id krävs",
+      status: 400,
+    };
 
-        const response = await fetch(`${host}/api/v1/bike/book`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
+    global.fetch.mockResolvedValue(mockResponse);
 
-        expect(response.status).toBe(400);
-        expect(response.message).toBe("Id och användare-id krävs");
+    const response = await fetch(`${host}/api/v1/bike/book`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bike),
     });
 
-    test("Should not be able to book a bike when missing id", async () => {
-        const bike = {
-            user: 1
-        };
-
-        const mockResponse = {
-            message: "Id och användare-id krävs",
-            status: 400,
-        };
-
-        global.fetch.mockResolvedValue(mockResponse);
-
-        const response = await fetch(`${host}/api/v1/bike/book`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bike),
-        });
-
-        expect(response.status).toBe(400);
-        expect(response.message).toBe("Id och användare-id krävs");
-    });
+    expect(response.status).toBe(400);
+    expect(response.message).toBe("Id och användare-id krävs");
+  });
 });

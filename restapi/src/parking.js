0bdb7d3d71c840ee3d9963e0e922302ba86c1cf4
Iwinter78 <kille1404.so@gmail.com>
2025-01-14 10:36:01 +0100

    Added comments to getParking function

--- restapi/src/parking.js
+++ restapi/src/parking.js
@@ -1,5 +1,9 @@
 import { connect } from "./connect.js";
 
+/**
+ * Gets all the parking zones from the database
+ * @returns {Array} - An array containing all parking zones
+ */
 async function allParking() {
   const db = await connect();
   const query = "CALL get_all_parking_zones()";
@@ -8,12 +12,4 @@ async function allParking() {
   return response[0];
 }
 
-async function updateAmountOfBikes(id, amount) {
-  const db = await connect();
-  const query = "CALL update_amount_of_bikes(?, ?)";
-  const response = await db.query(query, [id, amount]);
-  db.end();
-  return response[0];
-}
-
-export { allParking, updateAmountOfBikes };
+export { allParking };

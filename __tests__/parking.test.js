5822a0a3c74852de19d53de93ffc5301297207e2
Iwinter78 <kille1404.so@gmail.com>
2025-01-14 10:28:56 +0100

    Added test file for parking

---
+++ __tests__/parking.test.js
@@ -0,0 +1,19 @@
+/*
+import { jest, describe, expect, test, beforeEach } from "@jest/globals";
+const host = "http://localhost:1337";
+
+const mockResponse = [
+
+];
+
+describe("Parking features in rest api", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        global.fetch = jest.fn();
+    });
+
+    test("Be able to get all parking zones", async () => {
+
+    });
+});
+*/

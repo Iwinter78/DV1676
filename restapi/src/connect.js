ff1de8bbf168cca1007c172b06ee8ba95862c254
Iwinter78 <kille1404.so@gmail.com>
2025-01-17 10:40:54 +0100

    fix: now points towards database in docker

--- restapi/src/connect.js
+++ restapi/src/connect.js
@@ -6,12 +6,13 @@ import mysqlPromise from "mysql2/promise";
  */
 export async function connect() {
   const connect = mysqlPromise.createConnection({
-    host: "localhost",
+    host: "database",
     port: 3306,
     user: "admin",
     password: "123123",
     database: "magicbike",
     multipleStatements: true,
+    charset: "utf8mb4"
   });
 
   return Promise.resolve(connect);
